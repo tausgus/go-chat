78dd0519ed41d7129d3c5c5d348cdbd4490abb72
Gustavs Vagalis <gustavs.vagalis@protonmail.com>
2021-07-31 17:34:33 +0300

    Clients get added to the active connection list only after choosing a username - fixes messages getting in the way of username choice. Added join/leave messages for the clients. Implemented proper fix for disconnected clients still sending broadcasts to others.

--- main.go
+++ main.go
@@ -27,16 +27,31 @@ func main() {
 }
 
 func handleClient(client net.Conn) {
-	activeConnections = append(activeConnections, client) // Append new connection to the list of active ones, to be used for broadcasting messages
+	var isConnected bool = true
 
 	client.Write([]byte("Enter a username > "))
 	message, _ := bufio.NewReader(client).ReadString('\n') // Promt the new client for an username and map it to their IP address
 	users[client.RemoteAddr()] = string(message)
+	client.Write([]byte("Welcome, " + strings.TrimSuffix(users[client.RemoteAddr()], "\n") + ".\n"))
 
-	for {
+	activeConnections = append(activeConnections, client) // Append new connection to the list of active ones, to be used for broadcasting messages
+
+	for i := 0; i < len(activeConnections); i++ {
+		if activeConnections[i] != client { // Broadcast any new messages to all clients except the sender
+			activeConnections[i].Write([]byte("[SERVER] " + strings.TrimSuffix(users[client.RemoteAddr()], "\n") + " joined the room.\n"))
+		}
+	}
+
+	for isConnected {
 		message, _ := bufio.NewReader(client).ReadString('\n')
 
-		if string(message) != "" { // Ignore empty messages from disconnected clients
+		if message == "" { // Empty messages (no newlines) are sent by disconnected clients, this code will catch them and end the subroutine
+			for i := 0; i < len(activeConnections); i++ {
+				isConnected = false
+				client.Close()
+				activeConnections[i].Write([]byte("[SERVER] " + strings.TrimSuffix(users[client.RemoteAddr()], "\n") + " left the room.\n"))
+			}
+		} else {
 			for i := 0; i < len(activeConnections); i++ {
 				if activeConnections[i] != client { // Broadcast any new messages to all clients except the sender
 					activeConnections[i].Write([]byte("[" + strings.TrimSuffix(users[client.RemoteAddr()], "\n") + "]: " + message))
